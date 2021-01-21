fc15a9a28aa7e39ff16f1caf2feff5ffc7281a8f
mryash04 <yash.04may@gmail.com>
2021-01-21 12:24:28 +0530

    theme

---
+++ Theme/main.js
@@ -0,0 +1,13 @@
+const light = document.getElementById("light");
+const dark = document.getElementById("dark");
+const body = document.body;
+
+light.addEventListener("click", () => {
+    body.classList.add("light");
+    body.classList.remove("dark");
+});
+
+dark.addEventListener("click", () => {
+    body.classList.add("dark");
+    body.classList.remove("light");
+})
